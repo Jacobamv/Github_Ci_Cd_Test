24cb48835a69cbfcecb30e39e45b026dde15fcd5
Jacobamv <jecob.akhmedov@gmail.com>
2023-01-31 23:49:52 +0100

    golang code

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+import (
+    "fmt"
+    "net/http"
+    "github.com/gorilla/mux"
+	"encoding/json"
+)
+ 
+type Response struct{
+	Message  string `json:"message"`
+}
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{
+		Message: "test",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+ 
+func main() {
+      
+    router := mux.NewRouter()
+    router.HandleFunc("/test", TestHandler)
+    http.Handle("/",router)
+ 
+    fmt.Println("Server is listening...")
+    http.ListenAndServe(":8181", nil)
+}
