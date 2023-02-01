ecb556ec2c1860b8ffac4e7d95f0bb9aa21d6872
Jacobamv <jecob.akhmedov@gmail.com>
2023-02-01 17:00:16 +0100

    test

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ type Response struct{
 
 func TestHandler(w http.ResponseWriter, r *http.Request) {
 	response := Response{
-		Message: "123",
+		Message: "Another test",
 	}
 
 	w.Header().Set("Content-Type", "application/json")
